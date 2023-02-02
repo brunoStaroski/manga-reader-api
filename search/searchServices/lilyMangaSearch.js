0d5540d97e4c73d79c2c8de3d3e66477c08e4a15
Bruno Staroski <bruno.souza@credisfera.com.br>
2023-02-02 13:11:28 -0300

    - added a placeholder error handling in lilyMangaSearch.js

--- search/searchServices/lilyMangaSearch.js
+++ search/searchServices/lilyMangaSearch.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const pretty = require("pretty");
+//const pretty = require("pretty");
 
 const url = 'https://lilymanga.com/?s=';
 const postType = '&post_type=wp-manga'
@@ -11,6 +11,11 @@ module.exports = {
         let mangas = []
 
         await axios.get(`${url}${name}${postType}`).then(result => {
+            if (result.status !== 200 || !result.data || result.data === '') {
+                console.log(`Error while trying to search ${url}${name}${postType}`)
+                return mangas; //TODO better error handling
+            }
+
             const $ = cheerio.load(result.data);
             const searchResults = $('.tab-content-wrap > .c-tabs-item').children();
 
@@ -33,12 +38,12 @@ module.exports = {
                     status
                 });
 
-                console.log(idx, title,
-                    image,
-                    url,
-                    lastChapter,
-                    lastUpdatedOn,
-                    status)
+                // console.log(idx, title,
+                //     image,
+                //     url,
+                //     lastChapter,
+                //     lastUpdatedOn,
+                //     status)
             });
         })
         return mangas;
